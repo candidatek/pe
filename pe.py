ccfe0e5068b01bcfa4ecdd06c37e5caef06343f4
skshriharik <shrihariskulkarni8@gmail.com>
2020-07-22 01:46:14 +0530

    added index

--- pe.py
+++ pe.py
@@ -9,12 +9,14 @@ driver = webdriver.Chrome()
 f = open('output.txt','w')
 
 def browse(sector):   
-    print(sector)
+    #print(sector)
     driver.get(sector)
     time.sleep(2)
     sector_name = driver.find_element_by_xpath('/html/body/div[2]/div[3]/div[2]/div/div[1]/div/h6').text
     sector_pe = driver.find_element_by_xpath('/html/body/div[2]/div[3]/div[2]/div/div[1]/div/a/div').text
-    print(sector_name," - " ,sector_pe, file= f)
+    sector_value = driver.find_element_by_xpath('/html/body/div[2]/div[1]/div[2]/div[1]/div[2]/div[1]/span[1]').text
+    print(sector_name,":" ,sector_pe,"Index :",sector_value, file= f)
+    print(sector_name,":" ,sector_pe,"Index :",sector_value)
 
 
 browse(constants.NIFTY50LINK)
