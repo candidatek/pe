2fc023c58dd5348d1c98ad0b5a06dc72c1851781
skshriharik <shrihariskulkarni8@gmail.com>
2020-07-22 01:36:35 +0530

    intendation

--- pe.py
+++ pe.py
@@ -1,18 +1,17 @@
 #pip3 install selenium
 #sudo apt-get install chromium-chromedriver
 from selenium import webdriver
-#import details
 import constants  
 from os import path
 import time
 driver = webdriver.Chrome()
-inputfile = 'title.py'
+
 f = open('output.txt','w')
 
 def browse(sector):   
     print(sector)
     driver.get(sector)
-    # time.sleep(2)
+    time.sleep(2)
     sector_name = driver.find_element_by_xpath('/html/body/div[2]/div[3]/div[2]/div/div[1]/div/h6').text
     sector_pe = driver.find_element_by_xpath('/html/body/div[2]/div[3]/div[2]/div/div[1]/div/a/div').text
     sector_name.replace("PE" , "")
@@ -20,16 +19,18 @@ def browse(sector):
     print(sector_name," - " ,sector_pe, file= f)
 
 
-
-#NIFTYBANK = details.Ratio(constants.NIFTYBANK , constants.NIFTYBANKLINK)
+browse(constants.NIFTY50LINK)
+browse(constants.NIFTYNEXT50)
 browse(constants.NIFTYBANKLINK)
 browse(constants.NIFTYITLINK)
-browse(constants.NIFTYITLINK)
 
-file1 = open(inputfile, 'r')
-Lines = file1.readlines()
-for line in Lines:
-    print(line)
+
+
+#inputfile = 'title.py'
+# file1 = open(inputfile, 'r')
+# Lines = file1.readlines()
+# for line in Lines:
+#     print(line)
 
 
 driver.close()
